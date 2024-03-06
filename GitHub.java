d5978a2727f9dd378c28f680f92c0af741d848f7
AbhijithTyss <145775712+AbhijithTyss@users.noreply.github.com>
2024-03-06 11:49:42 +0530

    Update GitHub.java

--- GitHub.java
+++ GitHub.java
@@ -2,5 +2,6 @@ public class GitHub{
 	public static void main(String[] args){
 		System.out.println("GitHub practice");
 		System.out.println("GitHub practice modified");
+		System.out.println("GitHub practice modified2");
 	}
 }
